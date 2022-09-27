6031f3136ae9890198bce3736aedf76df75cf1fe
gayatrisathawane <gayatripsathawane@gmail.com>
2022-09-27 14:59:36 +0530

    update

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ qrImg=wrapper.querySelector(".qr-code img");
 generateBtn.addEventListener("click",()=>{
 let qrValue=qrInput.value;
 if(!qrValue) return;
-generateBtn.innerText="Generatin QR Code....";
+generateBtn.innerText="Generating QR Code....";
 qrImg.src=`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`;
 
 qrImg.addEventListener("load",()=>{
